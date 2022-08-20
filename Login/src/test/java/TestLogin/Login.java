6780685e6507bacda7aa3d0d301dcce90c3eaad5
claudiofergarcia <clau.fernando.garcia@gmail.com>
2022-08-20 20:03:31 -0300

    Se agregan capturas de evidencias en docx

--- Login/src/test/java/TestLogin/Login.java
+++ Login/src/test/java/TestLogin/Login.java
@@ -1,14 +1,17 @@
 package TestLogin;
 
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import java.io.IOException;
 import java.time.Duration;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import Utilities.CaptureEvidence;
 import pages.PageLogin;
 import pages.PageLogon;
 import pages.PageProducts;
@@ -18,6 +21,10 @@ public class Login {
 	private String url = "https://www.saucedemo.com/";
 	private String driverPath = "..\\Login\\Driver\\chromedriver.exe";
 	private WebDriver driver;
+	private String evidenceRoute = "..\\Login\\Evidence\\";
+	private String documentName = "Evidence - Saucedemo.docx";
+	private String temporaryNameImg = "img.jpg";
+	
 	
 	@BeforeMethod
 	public void setUp() {
@@ -32,26 +39,33 @@ public class Login {
 	}
 	
 	@Test(priority = 0)
-	public void loginOk() {
+	public void loginOk() throws InvalidFormatException, IOException, InterruptedException {
+		
+		CaptureEvidence.writeTitle(evidenceRoute + documentName, "Documento de Evidencias", 20);
+		CaptureEvidence.takeAScreenshot(driver, evidenceRoute + temporaryNameImg , evidenceRoute + documentName, "Login");
 		
 		PageLogin pageLogin = new PageLogin(driver);
 		PageProducts pageProducts = new PageProducts(driver);
 		pageLogin.login("standard_user", "secret_sauce");
-	   
+		
 	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 	    wait.until(ExpectedConditions.
 	    elementToBeClickable(By.cssSelector("div.page_wrapper div:nth-child(1) div.header_container:nth-child(1) "
 	    		+ "div.header_secondary_container > span.title")));
 	    
+	    CaptureEvidence.takeAScreenshot(driver, evidenceRoute + temporaryNameImg , evidenceRoute + documentName, "Logueado Ok");
+	    
 	    pageProducts.assertLoginProducts();
 	}
 	
 	@Test(priority = 1)
-	public void loginNoOK() {
+	public void loginNoOK() throws InvalidFormatException, IOException, InterruptedException {
 		
 		PageLogin pageLogin = new PageLogin(driver);
 		PageLogon pageLogon = new PageLogon(driver);
 		pageLogin.login("user", "sauce");
+		
+		CaptureEvidence.takeAScreenshot(driver, evidenceRoute + temporaryNameImg , evidenceRoute + documentName, "Login NO OK");
 		
 		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
 	    wait.until(ExpectedConditions.elementToBeClickable(By.tagName("h3"))); 
